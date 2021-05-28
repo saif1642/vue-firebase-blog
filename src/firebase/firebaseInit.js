578a3afed5101f4957ae206fdac5830e32eae3e9
saif1642 <islumsaiful@gmail.com>
2021-05-28 11:46:19 +0600

    project setup and initial commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAZiGdXYUKF91xJLPIg7umVxPZOzs32eFU",
+    authDomain: "vuefireblog.firebaseapp.com",
+    projectId: "vuefireblog",
+    storageBucket: "vuefireblog.appspot.com",
+    messagingSenderId: "428224529518",
+    appId: "1:428224529518:web:274c992c09439eabecd27f",
+    measurementId: "G-QLB4KRPFFY"
+  };
