15c21cd4d5d21ace00f27ec452a8ff7c45e7c251
saif1642 <islumsaiful@gmail.com>
2021-09-06 12:44:57 +0600

    blog crad added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
      <BlogPost :post="welcomeScreen"/>
      <BlogPost :post="post" v-for="(post,index) in sampleBlogPost" :key="index"/>
+     <div class="blog-card-wrap">
+       <div class="container">
+         <h3>View More Recent Blogs</h3>
+         <div class="blog-cards">
+            <BlogCard :post="post" v-for="(post,index) in sampleBlogCards" :key="index"/>
+         </div>
+       </div>
+     </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost";
+import BlogCard from "../components/BlogCard";
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: {BlogPost,BlogCard},
   data(){
     return{
       welcomeScreen: {
@@ -30,8 +39,57 @@ export default {
           "blogHtml": "Javascript event loop explained",
           "blogCoverPhoto": "designed-for-everyone"
         }
+      ],
+      sampleBlogCards: [
+        {blogTitle:"Blog Card #1",blogCoverPhoto:"stock-1",blogDate:"May 1,2021"},
+        {blogTitle:"Blog Card #2",blogCoverPhoto:"stock-2",blogDate:"May 2,2021"},
+        {blogTitle:"Blog Card #3",blogCoverPhoto:"stock-3",blogDate:"May 3,2021"},
+        {blogTitle:"Blog Card #4",blogCoverPhoto:"stock-4",blogDate:"May 4,2021"}
       ]
     }
   }
 };
 </script>
+
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
