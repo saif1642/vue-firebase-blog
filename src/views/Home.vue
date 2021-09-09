5dd1f06b9a6516414f44cd2ac9b41fae36e6b8ca
saif1642 <islumsaiful@gmail.com>
2021-09-09 14:53:48 +0600

    blog cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -54,42 +54,29 @@ export default {
 
 <style lang="scss" scoped>
 .blog-card-wrap {
-  h3 {
-    font-weight: 300;
-    font-size: 28px;
-    margin-bottom: 32px;
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+  @media(min-width: 500px) {
+    padding: 100px 16px;
   }
-}
-.updates {
-  .container {
-    padding: 100px 25px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    @media (min-width: 800px) {
-      padding: 125px 25px;
-      flex-direction: row;
+
+  .blog-cards {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 1fr;
+    @media (min-width: 500px) {
+      grid-template-columns: repeat(2, 1fr);
     }
-    .router-button {
-      display: flex;
-      font-size: 14px;
-      text-decoration: none;
-      @media (min-width: 800px) {
-        margin-left: auto;
-      }
+
+    @media (min-width: 900px) {
+      grid-template-columns: repeat(3, 1fr);
     }
-    h2 {
-      font-weight: 300;
-      font-size: 32px;
-      max-width: 425px;
-      width: 100%;
-      text-align: center;
-      text-transform: uppercase;
-      @media (min-width: 800px) {
-        text-align: initial;
-        font-size: 40px;
-      }
+
+    @media (min-width: 1200px) {
+      grid-template-columns: repeat(4, 1fr);
     }
   }
 }
+
 </style>
