eb367c4bbe3a64d11802a5c725b22155853d1609
saif1642 <islumsaiful@gmail.com>
2021-09-10 13:02:43 +0600

    register section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,14 @@
          </div>
        </div>
      </div>
+     <div class="updates">
+       <div class="container">
+         <h2>Never miss a post. Register for free!</h2>
+         <router-link class="router-button" to="#">
+           Register for VueFiredBlog <Arrow class="arrow arrow-light"/>
+         </router-link>
+       </div>
+     </div>
   </div>
 </template>
 
@@ -75,6 +83,39 @@ export default {
 
     @media (min-width: 1200px) {
       grid-template-columns: repeat(4, 1fr);
+    }
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
     }
   }
 }
